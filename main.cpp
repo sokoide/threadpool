4ab5d2336d92f9e124eda0eada1308c142eae40c
So Koide <sokoide@gmail.cojm>
2024-05-28 23:24:36 +0900

    refactor: generalized

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "pool.h"
 
 using namespace std;
-// using namespace atcoder;
 
 using ll = long long;
 using ll = long long;
@@ -13,43 +12,12 @@ using pll = pair<ll, ll>;
 
 ThreadPool pool;
 
-// void init_thread_pool(size_t num_threads) {
-//     for (size_t i = 0; i < num_threads; ++i) {
-//         threads.emplace_back(std::thread([]() {
-//             while (true) {
-//                 // Get a task from the task queue
-//                 // ...
-//             }
-//         }));
-//     }
-// }
-//
-// template <typename F, typename... Args>
-// auto push_task(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
-//     std::packaged_task<decltype(f(args...))()> task(
-//         std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-//     auto future = task.get_future();
-//     // Push the task to the task queue
-//     // ...
-//     {
-//         std::unique_lock<std::mutex> lock(queue_mutex);
-//         jobs.push(task, args...);
-//     }
-//     mutex_condition.notify_one();
-//     return future;
-// }
-//
-// int compute_result(int x, int y) {
-//     std::this_thread::sleep_for(std::chrono::seconds(2)); // Simulate some
-//     work return x + y;
-// }
-
 int main() {
     pool.Start(3);
     vector<future<int>> v;
 
     for (int i = 0; i < 5; i++) {
-        future<int> f = pool.Queue([i]() -> int {
+        auto f = pool.Queue([i] {
             using namespace chrono_literals;
             this_thread::sleep_for(1s);
             auto now = chrono::system_clock::now();
--- pool.h
+++ pool.h
@@ -9,17 +9,89 @@
 
 using namespace std;
 
+// uncomment if you want to use pthread
 // #define USE_PTHREAD
 
 class ThreadPool {
   public:
-    void Start(int num_threads);
-    void Stop();
-    future<int> Queue(const function<int()>& task);
+    void Start(int num_threads) {
+        for (int i = 0; i < num_threads; i++) {
+#ifdef USE_PTHREAD
+            pthread_t pt;
+            pthread_create(&pt, nullptr, ThreadPool::LoopWrapper, this);
+            threads.emplace_back(pt);
+#else
+            threads.emplace_back(thread(&ThreadPool::Loop, this));
+            ;
+#endif
+        }
+    }
+
+    void Stop() {
+        {
+            unique_lock<mutex> lock(mtx);
+            should_terminate = true;
+        }
+        cond.notify_all();
+#ifdef USE_PTHREAD
+        for (auto& active_thread : threads) {
+            pthread_join(active_thread, nullptr);
+        }
+#else
+        for (auto& active_thread : threads) {
+            active_thread.join();
+        }
+#endif
+        threads.clear();
+    }
+
+    template <typename F, typename... Args>
+    inline auto Queue(F&& f,
+                      Args&&... args) -> std::future<decltype(f(args...))> {
+        // func with bounded parameters to make it ready to execute
+        std::function<decltype(f(args...))()> func =
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        // shared ptr to be able to copy construct / assign
+        auto task_ptr =
+            std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+
+        // wrap the packaged_task
+        std::function<void()> wrapper_func = [task_ptr]() { (*task_ptr)(); };
+        {
+            unique_lock<mutex> lock(mtx);
+            tasks.emplace(wrapper_func);
+        }
+
+        cond.notify_one();
+        return task_ptr->get_future();
+    }
 
   private:
-    void Loop();
-    static void* LoopWrapper(void* p);
+    void Loop() {
+        while (true) {
+            std::function<void()> task;
+            {
+                unique_lock<mutex> lock(mtx);
+                cond.wait(lock, [this] {
+                    return !tasks.empty() || should_terminate;
+                });
+                if (should_terminate) {
+                    return;
+                }
+                if (tasks.empty())
+                    continue;
+                task = tasks.front();
+                tasks.pop();
+            }
+            task();
+        }
+    }
+
+    static void* LoopWrapper(void* p) {
+        reinterpret_cast<ThreadPool*>(p)->Loop();
+        return nullptr;
+    }
+
     bool should_terminate = false;
 
     mutex mtx;
@@ -29,5 +101,5 @@ class ThreadPool {
 #else
     vector<thread> threads;
 #endif
-    queue<packaged_task<int()>> tasks;
+    queue<function<void()>> tasks;
 };
